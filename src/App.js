28b608a7c34c90405e7ef7ddcbaa8c31a1060c3b
Павел <elantsev.pa@gmail.com>
2019-10-22 09:24:39 +0300

    +Navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import "./App.css";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms/" component={Rooms} />
-      <Route path="/rooms/:id" component={SingleRoom} />
-      <Route component={Error} />
-    </Switch>
+    <>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/" exact component={Rooms} />
+        <Route path="/rooms/:id" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
   );
 }
 
